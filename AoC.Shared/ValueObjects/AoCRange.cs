2ff89beba15e7c83d70910f013d55861315eaea3
Dominik Tyc <DominikTyc@proton.me>
2025-12-05 08:50:47 +0100

    2025 Day 5

---
+++ AoC.Shared/ValueObjects/AoCRange.cs
@@ -0,0 +1,19 @@
+﻿namespace AoC.Shared.ValueObjects;
+
+public record AoCRange(long Left, long Right)
+{
+    public static AoCRange CreateEmpty() 
+        => new(0, 0);
+
+    public bool IsEmpty()
+        => Left == 0 && Right == 0;
+
+    public bool IsInRange(long v) 
+        => Left <= v && Right >= v;
+
+    public bool IsInRange(AoCRange subRange) 
+        => Left <= subRange.Left && Right >= subRange.Right;
+
+    public long ValueCount() 
+        => Right - Left + 1;
+}
