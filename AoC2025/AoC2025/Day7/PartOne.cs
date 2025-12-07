ec00bdd500e57695514ce729c361fc9b77fbca20
Dominik Tyc <DominikTyc@proton.me>
2025-12-07 15:16:40 +0100

    2025 Day 7

---
+++ AoC2025/AoC2025/Day7/PartOne.cs
@@ -0,0 +1,38 @@
+using AoC.Shared;
+
+namespace AoC2025.Day7;
+
+public class PartOne(string input) : Solution(input)
+{
+    public override long Solve()
+    {
+        var tachylonManifoldsDiagram = File.ReadAllLines(Input)
+            .Select(x => x.ToCharArray())
+            .ToArray();
+
+        var beamYTracker = new List<int> { tachylonManifoldsDiagram[0].IndexOf('S') };
+        var splitTracker = 0L;
+
+        for (var y = 0; y < tachylonManifoldsDiagram.Length - 1; y++)
+        {
+            var newBeamsY = new List<int>();
+            foreach (var beam in beamYTracker)
+            {
+                if (tachylonManifoldsDiagram[y + 1][beam] == '^')
+                {
+                    newBeamsY.Add(beam - 1);
+                    newBeamsY.Add(beam + 1);
+                    splitTracker++;
+                }
+                else
+                {
+                    newBeamsY.Add(beam);
+                }
+            }
+
+            beamYTracker = [.. newBeamsY.Distinct()];
+        }
+
+        return splitTracker;
+    }
+}
---
+++ AoC2025/AoC2025/Day7/PartTwo.cs
@@ -0,0 +1,27 @@
+using AoC.Shared;
+using AoC.Shared.Helpers;
+
+namespace AoC2025.Day7;
+
+public class PartTwo(string input) : Solution(input)
+{
+    private char[][] _tachylonManifoldsDiagram = [];
+
+    public override long Solve()
+    {
+        _tachylonManifoldsDiagram = [.. File.ReadAllLines(Input).Select(x => x.ToCharArray())];
+
+        Func<int, int, long> sentParticle = null!;
+        sentParticle = FuncHelpers.Memoize((int beam, int y) =>
+        {
+            if (y == _tachylonManifoldsDiagram.Length - 1)
+                return 1;
+
+            return _tachylonManifoldsDiagram[y + 1][beam] == '^' // chekc if split beam occured
+                ? sentParticle(beam - 1, y + 1) + sentParticle(beam + 1, y + 1)
+                : sentParticle(beam, y + 1);
+        });
+
+        return sentParticle(_tachylonManifoldsDiagram[0].IndexOf('S'), 1);
+    }
+}
