1f7a53ed7fda018a8a0ae91159fedbb312fb0abb
Dominik Tyc <DominikTyc@proton.me>
2025-12-09 09:11:43 +0100

    2025 Day 9

---
+++ AoC.Shared/PlotHelper.cs
@@ -0,0 +1,18 @@
+﻿using AoC.Shared.ValueObjects;
+
+namespace AoC.Shared;
+
+public static class PlotHelper
+{
+    public static void SavePlot(Position2D[] p)
+    {
+        ScottPlot.Plot myPlot = new();
+        
+        var xs = p.Select(x => x.X).ToArray();
+        var xy = p.Select(y => y.Y).ToArray();
+
+        myPlot.Add.Scatter(xs, xy);
+
+        myPlot.SavePng("demo.png", 4000, 3000);
+    }
+}
---
+++ AoC2025/AoC2025/Day9/PartOne.cs
@@ -0,0 +1,35 @@
+using AoC.Shared;
+using System.Collections.Immutable;
+
+namespace AoC2025.Day9;
+
+public class PartOne(string input) : Solution(input)
+{
+    public override long Solve()
+    {
+        var redTilesPositions = File.ReadAllLines(Input)
+            .Select(x => x.Split(",").Select(int.Parse).ToArray())
+            .ToImmutableArray();
+
+        var maxSize = 0L;
+
+        for (var i = 0; i < redTilesPositions.Length - 1; i++)
+        {
+            for (var j = i + 1; j < redTilesPositions.Length; j++)
+            {
+                long xL = GetTileLength(redTilesPositions[i][0], redTilesPositions[j][0]);
+                long yL = GetTileLength(redTilesPositions[i][1], redTilesPositions[j][1]);
+
+                var square = xL * yL;
+
+                if (maxSize < square)
+                    maxSize = square;
+            }
+        }
+
+        return maxSize;
+    }
+
+    private static long GetTileLength(int t1, int t2) 
+        => t1 > t2 ? t1 - t2 + 1 : t2 - t1 + 1;
+}
